d8b3e666a4464d8193937a16fcfd6ac336393712
Christine Markwalter <christine.markwalter@duke.edu>
2023-02-08 10:50:58 -0500

    update identify_matches

--- scripts/identify_matches.R
+++ scripts/identify_matches.R
@@ -1,27 +1,32 @@
+
+
 library(tidyverse)
 
-lrs <- read_csv('output/log10LRs.csv')
+###### read data in for one mosquito ######
+lrs <- read_csv("analysis/matching/generated_tables/log10LRs.csv") %>% 
+  filter(sample_evidence == "M07_0575")
 
-get_matches <- function(lrs, lr_thresh, norm_thresh){
-  lrs %>% 
+
+
+###### function to get matches from LRs for 1 mosquito at a given threshold ######
+
+get_matches_1moz <- function(moz_lrs, lr_thresh, norm_thresh){
+  moz_lrs %>% 
     # non-matches
-    group_by(sample_evidence) %>% 
     mutate(all_bad = all(log10LR < lr_thresh | is.na(log10LR) | is.infinite(log10LR)),
            note = ifelse(all_bad & is.na(note), 'All log10LR < thresh', note),
            sample_reference = ifelse(all_bad, NA, sample_reference),
-           log10LR = ifelse(all_bad, NA, log10LR)) %>% 
-    distinct() %>% 
-    left_join(lrs_old) %>%
+           log10LR = ifelse(all_bad, NA, log10LR)) %>%
+    distinct() %>%
     # potential matches - ambiguous
     filter((log10LR >= lr_thresh & !is.infinite(log10LR)) | !is.na(note)) %>% 
-    group_by(sample_evidence) %>%
     mutate(prefilt_lr_norm = log10LR/sum(log10LR)*min_noc, # use logs or not? 
            all_norm_lt_thresh = all(prefilt_lr_norm < norm_thresh & is.na(note)),
            note = ifelse(all_norm_lt_thresh, 'Ambiguous matches only', note),
            sample_reference = ifelse(all_norm_lt_thresh, NA, sample_reference),
            log10LR = ifelse(all_norm_lt_thresh, NA, log10LR),
            prefilt_lr_norm = ifelse(all_norm_lt_thresh, NA, prefilt_lr_norm)) %>% 
-    distinct() %>% 
+    distinct() %>%
     # potential matches - less ambiguous
     filter(prefilt_lr_norm >= norm_thresh | !is.na(note)) %>%
     mutate(n_refs_gt_thresh = n_distinct(sample_reference[log10LR > lr_thresh])) %>%
@@ -32,40 +37,87 @@ get_matches <- function(lrs, lr_thresh, norm_thresh){
            match = case_when(note == 'Passed all filters' ~ 'Yes',
                              note == '> min NOC matches' ~ 'Maybe',
                              TRUE ~ 'No')
-           ) %>% 
-    select(sample_evidence, min_noc, mlocus_count, match, match_dbl, sample_reference, log10LR, note)
+    ) %>%
+    select(sample_evidence, min_noc, m_locus_count, match, match_dbl, sample_reference, log10LR, note)
 }
 
 
-lr_thresh_options <- lapply(seq(0, 10, by = 0.5), function(thresh){
-  matches <- get_matches(lrs = lrs, lr_thresh = thresh, norm_thresh = 0.5) 
-  hu_matches <- matches %>% 
-    group_by(match) %>% 
-    summarize(hu_matches = n())
-  mosq_matches <- matches %>% 
-    select(sample_evidence, match) %>%
-    distinct() %>% 
-    group_by(match) %>% 
-    summarize(mosq_matches = n())
-  full_join(hu_matches, mosq_matches) %>% 
-    mutate(lr_thresh = thresh, .before = 1) 
-}) %>% bind_rows() %>% 
-  pivot_wider(names_from = match, values_from = c(hu_matches, mosq_matches)) %>% 
-  rename(no = hu_matches_No, 
-         mosq_yes = mosq_matches_Yes,
-         hu_yes = hu_matches_Yes,
-         mosq_maybe = mosq_matches_Maybe,
-         hu_maybe = hu_matches_Maybe) %>% 
-  select(lr_thresh, no, mosq_yes, hu_yes, mosq_maybe, hu_maybe) %>% 
-  suppressMessages()
 
-optim_lr_thresh <- lr_thresh_options %>% 
-  slice_max(hu_yes) %>% 
-  slice_max(mosq_yes) %>% 
-  slice_max(lr_thresh) %>% 
-  pull(lr_thresh)
+##### Screen the log10LR thresholds #####
 
-lr_thresh_options <- lr_thresh_options %>% 
-  mutate(optim_lr_thresh = ifelse(lr_thresh == optim_lr_thresh, 'Yes', 'No'))
+  #set initial match conditions
+  
+  thresh = 10.5
 
-matches <- get_matches(lrs, lr_thresh = optim_lr_thresh, norm_thresh = 0.5)
+  matches <- get_matches_1moz(moz_lrs = lrs, lr_thresh = thresh, norm_thresh = 0.5) %>%
+    select(-match_dbl) %>%
+    mutate(thresh_low = thresh)
+  
+  mht <- matches %>%
+    pull(sample_reference)
+  
+  mlt <- "placeholder"
+  
+  df <- matches %>%
+    mutate(thresh_low = 10.5)
+  
+  thresh = 10
+  
+  # screen thresholds
+  while(!identical(mht, mlt)){
+    matches <- get_matches_1moz(moz_lrs = lrs, lr_thresh = thresh, norm_thresh = 0.5) %>%
+      select(-match_dbl) %>%
+      mutate(thresh_low = thresh)
+    
+    df <- df %>%
+      bind_rows(matches)
+    
+    mlt <- matches %>%
+      pull(sample_reference)
+    
+    if(identical(mht, mlt) & nrow(matches) >= matches$min_noc[1] & !all(is.na(mlt))) {
+      break
+    }
+    
+    thresh = thresh - 0.5
+    
+    mht <- mlt
+    mlt <- "placeholder"
+    
+    if(thresh == 0.5){
+      break
+    }
+    
+  }
+   
+  if(nrow(matches) < matches$min_noc[1]){
+  
+    temp <- df %>%
+      group_by(thresh_low) %>%
+      mutate(sample_reference = str_c(sample_reference, collapse = ","),
+             log10LR = str_c(log10LR, collapse = ",")) %>%
+      ungroup() %>%
+      distinct() %>%
+      filter(!is.na(sample_reference)) %>%
+      arrange(thresh_low) %>%
+      mutate(next_same = sample_reference == lead(sample_reference) & note == lead(note)) %>% 
+      filter(next_same == TRUE) %>%
+      slice_max(thresh_low) %>%
+      separate_rows(sample_reference, log10LR, sep = ",") %>%
+      mutate(log10LR = as.numeric(log10LR)) %>%
+      select(-next_same)
+    
+    if(nrow(temp) > 0){
+      matches <- temp
+    }
+      
+  }
+
+
+##### save matches df as a .csv ####
+
+
+
+
+
+
